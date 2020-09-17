3b4e91ecc42cce5d96267c0603be1c16bd2f7f3f
Sogolumbo <33571916+Sogolumbo@users.noreply.github.com>
2020-09-17 08:06:31 +0200

    Change AssemblyInfo
    Adopt name changes

--- Playlist-Converter/Properties/AssemblyInfo.cs
+++ Playlist-Converter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Playlist-Converter")]
+[assembly: AssemblyTitle("PlaylistSyncLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Playlist-Converter")]
+[assembly: AssemblyProduct("Playlist-Sync")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
